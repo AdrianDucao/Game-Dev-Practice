e50b5e35713edd7665b065dbfb95c75f2b2dc1fb
AdrianDucao <adrianducao@gmail.com>
2020-03-22 12:02:54 +0800

    code update

--- Covid-19/KinematicBody2D.gd
+++ Covid-19/KinematicBody2D.gd
@@ -1,13 +1,22 @@
 extends KinematicBody2D
 #movements
+const UP = Vector2(0, -1)
 var motion = Vector2()
+var speed = 200
+var gravity = 10
+var jump = -400
 
 func _physics_process(delta):
+	motion.y += gravity
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = speed
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -speed
 	else:
 		motion.x = 0
 	
-	move_and_slide(motion)
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			motion.y = jump
+	
+	move_and_slide(motion, UP)
